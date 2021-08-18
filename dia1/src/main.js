5352998cccecc35478298d3b1e6e35d19e03655d
silasmasay <silasmasay19@hotmail.com>
2021-08-18 12:16:32 -0300

    dia1 - desafio 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,16 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const view = document.querySelector('[data-js="app"]');
+const link = document.querySelector('[data-js="link"]');
+
+view.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+`;
+
+
+link.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  view.classList.toggle('is-active');
+});
