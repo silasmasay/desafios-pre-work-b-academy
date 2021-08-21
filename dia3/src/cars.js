3f4228428280ddd0f988adb137c8125c6ede8656
silasmasay <silasmasay19@hotmail.com>
2021-08-21 17:05:13 -0300

    dia3 - desafio 3

---
+++ dia3/src/cars.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('[data-js="form-cars"]');
+const tbody = document.querySelector('[data-js="table-cars"]');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+
+  const image = this.elements.image;
+  const mark = this.elements.mark;
+  const model = this.elements.model;
+  const year = this.elements.year;
+  const plate = this.elements.plate;
+  const color = this.elements.color;
+
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td><img src="${image.value}" alt="Foto do Carro"/></td>
+    <td>${mark.value}</td>
+    <td>${model.value}</td>
+    <td>${year.value}</td>
+    <td>${plate.value}</td>
+    <td>${color.value}</td>
+  `;
+
+  this.reset();
+  image.focus();
+
+  tbody.appendChild(tr);
+});
--- dia3/src/main.js
+++ dia3/src/main.js
@@ -1,3 +1,4 @@
 import './style.css';
 
 import './form';
+import './cars';
